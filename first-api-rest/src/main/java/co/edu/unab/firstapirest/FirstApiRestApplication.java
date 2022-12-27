4a8140ab3ebeccd78d10dc6779c31857ae21945c
app <jeny.metaute.mt@usa.edu.co>
2022-12-27 08:09:06 -0500

    Initial commit

---
+++ first-api-rest/src/main/java/co/edu/unab/firstapirest/FirstApiRestApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unab.firstapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApiRestApplication.class, args);
+	}
+
+}
---
+++ first-api-rest/src/test/java/co/edu/unab/firstapirest/FirstApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unab.firstapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
